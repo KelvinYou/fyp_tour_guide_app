247cdda3a5e222333f8b5cbd9e1612d56b571fe5
KelvinYou <ykekelvin0220@gmail.com>
2022-09-12 20:40:10 +0800

    first push

---
+++ lib/bottom_bar_view.dart
@@ -0,0 +1,114 @@
+import 'dart:math' as math;
+
+import 'package:fyp_project/main.dart';
+import 'package:fyp_project/app_theme.dart';
+
+import 'package:flutter/material.dart';
+
+
+class BottomBarView extends StatefulWidget {
+  const BottomBarView({super.key});
+
+  @override
+  State<BottomBarView> createState() => _MyBottomBarView();
+}
+
+class _MyBottomBarView extends State<BottomBarView> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  // static const List<Widget> _widgettitles = <Widget>[
+  //   Text(
+  //     'Home',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Request',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Wallet',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Message',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Profile',
+  //     style: optionStyle,
+  //   ),
+  // ];
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Request',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Wallet',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Message',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+        // title: Center (
+        //   child: _widgettitles.elementAt(_selectedIndex),
+        // )
+      // ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.request_page),
+            label: 'Request',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Message',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import 'package:fyp_project/app_theme.dart';
+import 'package:fyp_project/bottom_bar_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]).then((_) => runApp(const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Tour Guide App",
+      home: BottomBarView(),
+    );
+  }
+}
+
+
+
